6edb50f7a9d595bcf3d76a19e79957d0efe71118
Timothy Cevallos <timcev@gmail.com>
2017-10-07 01:36:26 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -5,7 +5,15 @@ function init() {
   let index = 0
 
   document.body.addEventListener('keydown',function(e){
-
+    let key = parseInt(e.which || e.detail);
+    if (code[index] === key)
+      index++;
+      if (index === code.length) {
+        alert('Hurray!');
+      }
+      index = 0;
   })
-
+  else {
+    index = 0
+  }
 }
